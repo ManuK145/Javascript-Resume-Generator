019680f7604c9986134251db0be35265fe773354
ManuK145 <manughotra145@icloud.com>
2023-01-10 14:57:51 +0000

    Added the feature for selecting and uploading a photo to cv

--- script.js
+++ script.js
@@ -61,6 +61,17 @@ function generateCV(){
     }
     document.getElementById("acedemicQualificationT").innerHTML=str1;
 
+    let file=document.getElementById("imgField").files[0];
+    console.log(file);
+    let reader= new FileReader();
+    reader.readAsDataURL(file);
+    console.log(reader.result);
+    reader.onloadend=function(){
+        document.getElementById("imgTemplate").src=reader.result;
+    }
+   
+
+
     document.getElementById("cv-form").style.display="none";
     document.getElementById("cv-template").style.display="block";
 }   
